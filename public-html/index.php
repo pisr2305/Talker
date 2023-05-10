50ad5a7355fadeeee94e35709e51e4dc9f4a441e
pisr2305 <sreerajpillai23@gmail.com>
2023-05-10 23:00:43 +0530

    Bootstrap starter template

--- public-html/index.php
+++ public-html/index.php
@@ -1,177 +1,22 @@
-<?php
-$website = 'https://www.esntls.co/';
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALKER</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>TALKER</h1>
 
-
-<!DOCTYPE html>  
-<head>  
- <title>Hello World!</title>
-</head>
-
-<body>  
- <h1>I am Sreeraj</h1>
- <a href = "<?php echo $website; ?>" ><?php echo "Jose zuniga:  $website"; ?></a>
- <?php
- $truevalue = true;
- $falsevalue = false;
- ?>
- <p><?php echo "This is the content of truevalue: $truevalue";?></p>
- <p><?php echo "This is the content of falsevalue: $falsevalue";?></p>
- <p><?php echo strlen ('Hello World!');?></p>
- <p>
-    <?php
-      $countries = array('India' ,'America', 'UK');
-      print_r ($countries);
-      $countries[] = 'Italy';
-      print_r($countries);
-      ?>
-  </p>
-   <p>
-    <?php
-     echo $countries[1];
-     ?>
-    </p>
-    <p>
-      <?php
-      echo count($countries);
-      ?>
-      </p>
-    <p>
-     <?php
-     $age = array(
-        'John'=> 25,
-        'Sherlock' => 15,
-        'Mycroft' => 10
-     );
-      print_r($age);
-     ?>
-    </p>
-    <p>
-      <?php
-      $fighters = array(
-        'Mike Tyson' => 30,
-        'Donnie Yen' => 50,
-        'Scott Adkins' => 28
-      );
-      print_r($fighters);
-      ?>
-    </p>
-    <p>
-      <?php
-      echo $age['Sherlock'];
-      ?>
-      </p>
-    <script>
-      var cars = ["Mercedes", "Volvo", "BMW"];
-      for (i in cars) {
-        console.log("The current car is" + cars[i]);
-      }    
-    </script>
-
-    <?php
-     $cars = ["Mercedes", "Volvo", "BMW"];
-     foreach ($cars as$i) {
-        echo "The current car is $i</br>";
-     }
-     ?>
-    <p>
-      <?php
-       class carblueprint {
-          // here goes properties and methods
-          
-          // constructor
-           public function __construct($NEWCOLOR, $NEWMAKE) {
-             $this->color = $NEWCOLOR;
-             $this->make = $NEWMAKE;
-           }
-        
-          // SETTER METHOD 
-          public function setColor($NEWCOLOR) {
-            $this->color = $NEWCOLOR;
-          }
-            //GETTER METHOD
-          public function getColor()  {
-             return "<br>New color is:  "  . $this->color . "<br>";
-          }
-        
-        
-        
-        
-        
-        
-        
-
-
-
-
-
-        }
-
-         $firstrealcar = new carblueprint('green' , 'Volvo');
-         
-
-          var_dump($firstrealcar);
-          echo $firstrealcar->color;
-
-
-          
-            echo $firstrealcar->getColor();
-
-          $secondrealcar = new carblueprint('brown', 'Mercedes');
-          
-            echo $secondrealcar->getColor();
-             var_dump($secondrealcar);
-             
-
-         ?>
-    </p>
-   <p>
-    <?php
-      class sportscarblueprint extends carblueprint {
-          
-
-        //constructor
-         public function __construct($NEWCOLOR, $NEWMAKE, $NEWSPOILER) {
-            parent::__construct($NEWCOLOR, $NEWMAKE);
-            $this->spoiler = $NEWSPOILER;
-         }
-
-        public function activatespoiler() {
-          return "<br><strong>SPOILER ACTIVE!</strong></br>";
-         }
-        }
-
-        $firstsportscar = new sportscarblueprint('magenta', 'SF90', 'tail');
-        $firstsportscar-> setColor("PINK");
-        var_dump($firstsportscar);
-        $firstsportscar->activatespoiler();
-        ?>
-      </p>
-       <p>
-         <?php
-
-         function divideonebynumber($number)  {
-              if ($number == 0)  {
-                throw new exception("Division by zero is not allowed.");
-              }
-             return 1/$number;
-               
-            
-            
-            } 
-            
-            try {
-            echo "The result of division is: " . divideonebynumber(0);
-            }
-
-            catch (exception  $e) {
-              echo 'Message: ' . $e->getMessage();
-            }
-
-          ?>
-
-          </p>
-
-
- </body>
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
 
